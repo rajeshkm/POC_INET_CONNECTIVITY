36ea5a7ef3b0eab87ddcb047d054b36f36820ffc
Rajesh <rajesh.km@gmail.com>
2014-05-17 10:51:36 -0400

    Adding Project Files

---
+++ gen/com/example/poc_inet_connectivity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.poc_inet_connectivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/poc_inet_connectivity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.poc_inet_connectivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		ConnectionDetector cd = new ConnectionDetector(getApplicationContext());
+		
+		Boolean isInternetPresent = cd.isConnectingToInternet(); // true or false
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
